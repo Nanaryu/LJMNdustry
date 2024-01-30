89693c38d7a8830f94ffc02ea5a83f1a8ff0512e
Nanaryu <filipqqq1234@gmail.com>
2024-01-30 17:45:02 +0100

    update

--- game.js
+++ game.js
@@ -7,16 +7,35 @@ function line(x, y, x1, x2) {
     c.lineTo(x1, x2)
     c.stroke()
 }
-var cells = []
+var cells = [[]]
 
-for (i=0;i<1000;i+=100) {
-    for (j=0;j<1000;j+=100) {
-        line(j, 0, j, 1000)
+/* cells = [
+    [state, state, state], row 1
+    [state, state, state], row 2
+    [state, state, state]  row 3
+] */
+
+function draw_grid(grid_size, cell_size) {
+    let x = 0
+    let y = 0
+    let rows = grid_size / cell_size
+    for (i = 0; i < rows; i += 1) {
+        cells[i] = []
     }
-    line(0, i, 1000, i)
+
+    for (i = 0; i < grid_size ; i += cell_size) {
+        line(0, i, grid_size, i) // horizontal lines
+
+        for (j = 0; j < grid_size; j += cell_size) {
+            line(j, 0, j, grid_size) // vertical lines
+            cells[x][y] = "cell_state"
+            y += 1
+        }
+        //cells += ["cell_one"]
+        x += 1
+    }
+    console.log(JSON.stringify(cells))
 }
-
-
 
 canvas.addEventListener("click", function(e){
     cell_x = Math.floor(e.x/100)
@@ -34,3 +53,10 @@ canvas.addEventListener("mousemove", (e) => {
     c.fillRect(cell_x*100,cell_y*100,100, 100)
     c.fillStyle = "blueviolet"
 })
+/* 
+setInterval(() => {
+    draw_grid(1000, 100)
+    //console.log(cells)
+}, 10) */
+
+draw_grid(1000, 100)
