8495a02f28b512d5faee73dea55fb3ba6ee1dcf9
Nanaryu <filipqqq1234@gmail.com>
2024-02-11 21:52:21 +0100

    update

--- game.js
+++ game.js
@@ -1,10 +1,13 @@
 var canvas = document.getElementById("grid")
 var c = canvas.getContext("2d")
-var g_size = 640
-var c_size = 64
+var g_size = 768
+var c_size = 48
+
 
 var tit = new Image()
 tit.src = "item-titanium.png"
+tit.height = 30
+tit.width = 30
 
 var conv1 = new Image()
 conv1.src = "conv1.png"
@@ -18,45 +21,60 @@ conv3.src = "conv3.png"
 var conv4 = new Image()
 conv4.src = "conv4.png"
 
+var grass = new Image()
+grass.src = "grass.png"
+
+var coll = new Image()
+coll.src = "collector.png"
+
+var spawn = new Image()
+spawn.src = "spawner.png"
+
 var stats = 
 {
     titanium: 100,
+    power: 100,
 }
 
 var score = document.getElementById("score")
+var power = document.getElementById("power")
 
 var blocks =
 {
-    def_block: {
+    conveyor: {
         letter: "D",
         color: "gray",
         cost: 10,
+        name: "conveyor"
     },
-    free_block: {
+    freeblock: {
         letter: "F",
         color: "darkgreen",
         cost: 0,
+        name: "freeblock",
     },
-    pink_block: {
+    spawner: {
         letter: "P",
         color: "pink",
         cost: 80,
+        name: "spawner",
     },
-    coral_block: {
+    collector: {
         letter: "C",
         color: "lightcoral",
         cost: 10,
+        name: "collector",
     }
 }
 
 var rotation = 1
 
-var rotdecode = {
+/* var rotdecode = {
     1: "↑",
     2: "→",
     3: "↓",
     4: "←"
-}
+} */
 
 function velocity(x, y, rot)
 {
@@ -121,15 +139,14 @@ class Orb
                 }
                 else if (cells[this.cell_x][this.cell_y][0] == "C")
                 {   
-                    console.log(score.innerHTML.match(/[0-9]+/))
-                    score.innerHTML = `Score: ${parseInt(score.innerHTML.match(/[0-9]+/)) + this.score}`
+                    //score.innerHTML = `Score: ${parseInt(score.innerHTML.match(/[0-9]+/)) + this.score}`
+                    stats.titanium += this.score
                     this.x = -10
                     this.y = -10
                 }
                 c.fillStyle = this.fstyle
-                text(`Score: ${this.score}`, this.x, this.y + 15, 10)
-                circle(this.x, this.y, this.r)
-                c.drawImage(tit, this.x-tit.width/2, this.y-tit.height/2)
+                c.drawImage(tit, this.x-tit.width/2, this.y-tit.height/2, tit.width, tit.height)
+                text(`${this.score}`, this.x, this.y-2, 10)
             }
         }
         else
@@ -152,9 +169,10 @@ class Orb
             }
 
             c.fillStyle = this.fstyle
-            text(`Score: ${this.score}`, this.x, this.y + 15, 10)
-            circle(this.x, this.y, this.r)
-            c.drawImage(tit, this.x-tit.width/2, this.y-tit.height/2)
+        
+            c.drawImage(tit, this.x-tit.width/2, this.y-tit.height/2, tit.width, tit.height)
+            text(`${this.score}`, this.x, this.y-2, 10)
+
             if (this.x == this.dx && this.y == this.dy)
             {
                 this.moving = false
@@ -180,7 +198,7 @@ function spawnOrb()
                 let ii = (c_size/2)+i
                 let jj = (c_size/2)+j
 
-                if (x !=  g_size / c_size + 1)
+                if (x + 1 < g_size / c_size)
                 {
                     if (cells[x+1][y][0] == "D") // right
                     {
@@ -196,9 +214,9 @@ function spawnOrb()
                     }
                 }
             
-                if (y != g_size / c_size + 1)
+                if (y + 1 < g_size / c_size)
                 {
-                    if (cells[x][y+1][0] == "D") // up
+                    if (cells[x][y+1][0] == "D") // down
                     {
                         orbs.push(new Orb(ii, jj, c_size/8, ii, jj+c_size, true))
                     }
@@ -206,7 +224,7 @@ function spawnOrb()
 
                 if (y != 0) 
                 {
-                    if (cells[x][y-1][0] == "D") // down
+                    if (cells[x][y-1][0] == "D") // up
                     {
                         orbs.push(new Orb(ii, jj, c_size/8, ii, jj-c_size, true))
                     }   
@@ -232,44 +250,69 @@ function updateOrbPos()
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var current_block = blocks.free_block
+var current_block = blocks.freeblock
 
 var select = document.getElementById("select")
 
-var freeblock = document.createElement("div")
-freeblock.innerHTML = "free"
+var freeblock = document.createElement("img")
+freeblock.src = grass.src
 freeblock.id = "freeblock"
-freeblock.onclick = function () {current_block = blocks.free_block}
+freeblock.onclick = function () {current_block = blocks.freeblock}
 select.appendChild(freeblock)
 
-var spawner = document.createElement("div")
-spawner.innerHTML = "spawner"
+var spawner = document.createElement("img")
+spawner.src = spawn.src
 spawner.id = "spawner"
-spawner.onclick = function () {current_block = blocks.pink_block}
+spawner.onclick = function () {current_block = blocks.spawner}
 select.appendChild(spawner)
 
-var conveyor = document.createElement("div")
-conveyor.innerHTML = rotdecode[rotation]
+var conveyor = document.createElement("img")
+conveyor.src = conv1.src
 conveyor.id = "conveyor"
-conveyor.onclick = function () {current_block = blocks.def_block}
+conveyor.onclick = function () {current_block = blocks.conveyor}
 select.appendChild(conveyor)
 
-var collector = document.createElement("div")
-collector.innerHTML = "collector"
+var collector = document.createElement("img")
+collector.src = coll.src
 collector.id = "collector"
-collector.onclick = function () {current_block = blocks.coral_block}
+collector.onclick = function () {current_block = blocks.collector}
 select.appendChild(collector)
 
+var cost_span = document.getElementById("cost_span")
+
+var audio = new Audio('game3.mp3')
+audio.loop = true
+audio.volume = 0.4
+
+var music_span = document.getElementById("music_span")
+let music_on = false
+music_span.onclick = function () 
+{
+    if(!music_on) 
+    {
+        audio.play()
+        music_on = true
+        music_span.innerHTML = "ON"
+    } 
+    else 
+    {
+        audio.pause()
+        music_on = false
+        music_span.innerHTML = "OFF"
+    }
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-function line(x, y, x1, x2) 
+function line(x, y, x1, x2, fstyle="black") 
 {
     c.beginPath()
     c.moveTo(x, y)
     c.lineTo(x1, x2)
+    c.strokeStyle = fstyle
     c.stroke()
     c.closePath()
 }
@@ -280,7 +323,7 @@ function text(txt, x, y, fsize=32, fstyle="white")
     c.font = `${fsize}px Arial`
     c.fillStyle = fstyle
     c.textAlign = "center"
-    c.fillText(txt, x, y+7)
+    c.fillText(txt, x, y + 7)
     c.fillStyle = lastStyle
 }
 
@@ -300,12 +343,12 @@ function draw_grid()
 {
     for (i = 0; i < g_size ; i += c_size) 
     {
-        line(0, i, g_size, i) // horizontal lines
+        line(0, i, g_size, i, "rgba(233,233,233,0.3)") // horizontal lines
+    }
 
-        for (j = 0; j < g_size; j += c_size) 
-        {
-            line(j, 0, j, g_size) // vertical lines
-        }
+    for (j = 0; j < g_size; j += c_size) 
+    {
+        line(j, 0, j, g_size, "rgba(233,233,233,0.3)") // vertical lines
     }
 }
 
@@ -335,6 +378,11 @@ function draw_tiles()
             {
                 default:
                     break
+                case "F":
+                    {
+                        c.drawImage(grass, i, j, c_size, c_size)
+                        break
+                    }
                 case "D":
                     {   
                         switch(rot)
@@ -358,19 +406,56 @@ function draw_tiles()
                     break
                 case "P":
                     {
-                        text("spawner", i+c_size/2, j+c_size/2-3, 16, "black")
+                        //text("spawner", i+c_size/2, j+c_size/2-3, 16, "black")
+                        c.drawImage(spawn, i, j, c_size, c_size)
+                        break
                     }
-                    break
                 case "C":
                     {
-                        text("collector", i+c_size/2, j+c_size/2-3, 16, "black")
+                        //text("collector", i+c_size/2, j+c_size/2-3, 16, "black")
+                        c.drawImage(coll, i, j, c_size, c_size)
+                        break
                     }
-                    break
             }
             y += 1
         }
         y = 0
         x += 1
+    }
+}
+
+function selectedBlock()
+{
+    let selchild = select.children
+    for (let i = 0; i < selchild.length; i++)
+    {
+        if (selchild[i].id == current_block.name) {
+            selchild[i].style.boxShadow =  "0 0 10px cyan"
+        }
+        else
+        {
+            selchild[i].style.boxShadow = "none"
+        }
+    }
+}
+
+function updateStats()
+{
+    score.innerHTML = stats.titanium
+    power.innerHTML = `${stats.power}%`
+    cost_span.innerHTML = current_block.cost
+    if (current_block.cost <= stats.titanium)
+    {
+        cost_span.style.color = "lightgreen"
+    }
+    else
+    {
+        cost_span.style.color = "rgb(237, 84, 84)"
+    }
+    if (stats.power == 0) 
+    {
+        cells = null
+        alert("game over")
     }
 }
 
@@ -402,8 +487,10 @@ function init()
     }
 }
 
-function update() 
-{
+function frame() 
+{   
+    updateStats()
+    selectedBlock()
     draw_tiles()
     updateOrbPos()
     draw_grid()
@@ -413,18 +500,67 @@ function update()
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-canvas.addEventListener("click", function(e)
+var mouseheld = false
+canvas.addEventListener("mousedown", function(e)
 {
-    cell_x = Math.floor(e.offsetX/c_size)
-    cell_y = Math.floor(e.offsetY/c_size)
+    if (e.button === 0 && current_block.cost <= stats.titanium)
+    {
+        cell_x = Math.floor(e.offsetX/c_size)
+        cell_y = Math.floor(e.offsetY/c_size)
 
-    cells[cell_x][cell_y] = [current_block.letter, rotation]
+        if (cells[cell_x][cell_y][0] != current_block.letter || cells[cell_x][cell_y][1] != rotation)
+        {
+            stats.titanium -= current_block.cost
+            
+            Object.keys(blocks).forEach(key => {
+                if (cells[cell_x][cell_y][0] == blocks[key].letter)
+                {
+                    stats.titanium += blocks[key].cost
+                }
+            })
 
-
-    c.fillStyle = current_block.color
-    c.fillRect(cell_x * c_size, cell_y * c_size, c_size, c_size)
+            cells[cell_x][cell_y] = [current_block.letter, rotation]
+        }
+        
+        mouseheld = true
+    }
+    
+    //c.fillStyle = current_block.color
+    //c.fillRect(cell_x * c_size, cell_y * c_size, c_size, c_size)
 })
 
+canvas.addEventListener("mouseup", function(e)
+{
+    if (e.button === 0)
+    {
+        mouseheld = false
+    }
+})
+
+canvas.addEventListener("mousemove", function(e)
+{
+    if (mouseheld && current_block.cost <= stats.titanium)
+    {
+        cell_x = Math.floor(e.offsetX/c_size)
+        cell_y = Math.floor(e.offsetY/c_size)
+
+        if (cells[cell_x][cell_y][0] != current_block.letter || cells[cell_x][cell_y][1] != rotation)
+        {
+            stats.titanium -= current_block.cost
+
+            Object.keys(blocks).forEach(key => {
+                if (cells[cell_x][cell_y][0] == blocks[key].letter)
+                {
+                    stats.titanium += blocks[key].cost
+                }
+            })
+
+            cells[cell_x][cell_y] = [current_block.letter, rotation]
+        }
+    }
+})
+
+var spawnToggle = false
 window.addEventListener("keydown", function (e)
 {
     if (e.key === "r")
@@ -437,11 +573,34 @@ window.addEventListener("keydown", function (e)
         {
             rotation = 1
         }
-        conveyor.innerHTML = rotdecode[rotation]
+        switch (rotation)
+        {
+            case 1:
+            {
+                conveyor.src = conv1.src 
+                break
+            }
+            case 2:
+            {
+                conveyor.src = conv2.src 
+                break
+            }
+            case 3:
+            {
+                conveyor.src = conv3.src 
+                break
+            }
+            case 4:
+            {
+                conveyor.src = conv4.src 
+                break
+            }
+                
+        }
     }
     else if (e.key === "e")
     {
-        spawnOrb()
+        spawnToggle = spawnToggle ? false : true
     }
 })
 
@@ -454,15 +613,25 @@ const animate = () => {
 
   c.clearRect(0, 0, canvas.width, canvas.height)
 
-  update()
+  frame()
 }
 
 init()
 animate()
+setInterval(() => {
+    if (stats.power > 0 && spawnToggle)
+    {
+        spawnOrb()
+        stats.power -= 1
+    }
+}, 500)
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// block preview, did not work, dont want do to it any more
 
 /* var last_x = 99
 var last_y = 99
@@ -481,26 +650,24 @@ canvas.addEventListener("mousemove", function(e)
 
     if (((cell_x != last_x) || (cell_y != last_y)) && !trigger) 
     {
-        cells[cell_x][cell_y] = `H`
+        let lastCellState = cells[cell_x][cell_y]
+        cells[cell_x][cell_y] = current_block.letter
 
         last_x = cell_x
         last_y = cell_y
 
         c.fillStyle = current_block.color
-        c.fillRect(cell_x * c_size, cell_y * c_size, c_size, c_size)
-
-        draw_grid(g_size, c_size)
 
         for (let i = 0; i < cells.length; i++) 
         {
             for (let j = 0; j < cells[i].length; j++)
             {
-                if (cells[i][j] == "H" && ((i != cell_x) || (j != cell_y)))
+                if (cells[i][j] == current_block.letter && ((i != cell_x) || (j != cell_y)))
                 {
-                    cells[i][j] = "F"
+                    cells[i][j] = lastCellState.letter
                     update(g_size, c_size)
                 }
             }
         }
-    } 
+    }
 }) */
